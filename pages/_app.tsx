8c32b770a75d6559a0554d13d33f87f991054787
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-09-22 01:25:43 +0100

    bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import { GlobalContextProvider } from '../contexts/GlobalContext';
 import theme from '../styles/theme';
+import { AppProps } from 'next/app';
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<GlobalContextProvider>
 			<ChakraProvider theme={theme}>
