12b0e4fab69e3035da8d737f6db1319b7be0f6aa
DawidWraga <dpwraga@gmail.com>
2023-04-13 18:33:33 +0100

    feat: update projcet content

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -9,7 +9,7 @@ export type Project = {
 	id: string;
 	githubUrl?: string;
 	demoUrl?: string;
-	carouselSlides?: CarouselSlide[];
+	carouselSlides?: (CarouselSlide & { alt?: any })[];
 	wideCard?: boolean;
 };
 
@@ -52,7 +52,48 @@ export const projects: Project[] = [
 		],
 	},
 	{ id: '2', name: 'title2', description: 'subtitle', Icon: '', image: '' },
-	{ id: '3', name: 'title4', description: 'subtitle', Icon: '', image: '' },
+	{
+		id: '3',
+		name: 'Company website',
+		description: 'subtitle',
+		Icon: '',
+		image: '',
+		carouselSlides: [
+			{
+				label: 'Design system',
+				img: '/images/axalt-web/design-system.gif',
+				description:
+					'Visually consistent brand identity and scalable UI architecture, achieved through custom-made components that are carefully crafted to be highly reusable, modular and flexible.',
+				alt: 'scroll through top to bottom',
+			},
+			{
+				label: 'Mobile first',
+				img: '/images/axalt-web/mobile-first.jpeg',
+				description:
+					'Responsive design and performance optimisation ensure an exceptional user experience for all screens, devices, and browsers.',
+				alt: 'open side nav, open login, close login, scroll to bottom',
+			},
+			{
+				label: 'Job application & contact forms',
+				img: '/images/axalt-web/job-application-contact-forms.jpeg',
+				description:
+					'Fully featured forms, including backend email services, error validation and user feedback, streamlining the hiring process and client communications.',
+				alt: 'type in form except for email, type in email, submit form, submit form with error, submit form with success',
+			},
+			{
+				label: 'Clients & testemonials',
+				img: '/images/axalt-web/client-endorsements.jpeg',
+				description:
+					"Showcase of Axalt's successful work with previous clients can be easily updated and maintained as a result of intentionally designed system architecture.",
+			},
+			{
+				label: 'Animations',
+				img: '/images/axalt-web/animations.jpeg',
+				description:
+					'Engaging custom-made animations elevate user engagement with a touch of motion, leaving a lasting impression of the brand.',
+			},
+		],
+	},
 	{
 		id: '4',
 		name: 'HR & Invoice management portal (Axalt ltd)',
